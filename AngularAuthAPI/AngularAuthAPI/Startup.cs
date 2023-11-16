788ba0c3681eba35f174dddd387be415fbe890f8
Jonas Cardoso <Jonaseal17@gmail.com>
2023-11-16 16:17:37 -0300

    backend

---
+++ AngularAuthAPI/AngularAuthAPI/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace AngularAuthAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get;  }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddCors();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
